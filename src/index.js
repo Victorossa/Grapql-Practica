c649a89c5f1d70a17f24d25d8dbc354f21104064
Victor <victor.ossa@hotmail.com>
2019-05-20 21:34:05 -0500

    Para verificar push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 //creo la instancia de mi servidor, en la constante app tengo todo lo de mi servidor
 const app = express();
+
 //ahora le digo a mi constante que escuche indicandole el puerto y que cuando inicie me de un mensaje
 app.listen(2800, () => console.log('Server on port 2800'));
